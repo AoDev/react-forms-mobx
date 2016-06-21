da43b89a8fd24692325d9a88a39fde157fb1a820
Kevin Punelle <kevin.purnelle@gmail.com>
2016-06-21 11:57:33 +0200

    feat: asForm; updateArray handler
    Easy way to push or pop elements from an array

--- src/components/asForm.js
+++ src/components/asForm.js
@@ -8,14 +8,30 @@ export default function asForm (MyComponent, formDataProp) {
     constructor (props) {
       super(props)
       this.updateProperty = this.updateProperty.bind(this)
+      this.updateArray = this.updateArray.bind(this)
     }
 
     updateProperty (key, value) {
       this.props[formDataProp][key] = value
     }
 
+    updateArray (key, value) {
+      const array = this.props[formDataProp][key]
+      const index = array.indexOf(value)
+
+      if (array.indexOf(value) > -1) {
+        array.splice(index, 1)
+      } else {
+        array.push(value)
+      }
+    }
+
     render () {
-      return <MyComponent {...this.props} updateProperty={this.updateProperty}/>
+      return (
+        <MyComponent {...this.props}
+            updateProperty={this.updateProperty}
+            updateArray={this.updateArray}/>
+      )
     }
   }
 }
